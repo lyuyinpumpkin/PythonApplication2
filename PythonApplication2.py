9db28190bfd64e3c0b1c4e039563daf6e9fc98d4
Y L <yin.lv@cn.fujikura.com>
2020-02-13 14:35:00 +0800

    添加项目文件。

---
+++ PythonApplication2.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+
+style = ttk.Style()
+style.map("C.TButton",
+    foreground=[('pressed', 'red'), ('active', 'blue')],
+    background=[('pressed', '!disabled', 'black'), ('active', 'white')]
+    )
+
+colored_btn = ttk.Button(text="Test", style="C.TButton").pack()
+
+root.mainloop()
+
